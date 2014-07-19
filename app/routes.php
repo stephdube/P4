3a08b5afa8df73eb48649a867b36e4526ca7baba
Steph Dube <scdube7@gmail.com>
2014-07-19 03:03:39 -0400

    loaded albums data into albums table in database

--- app/routes.php
+++ app/routes.php
@@ -23,6 +23,17 @@ Route::get('sql', function(){
 Route::get('albums', function(){
 	$albums = file_get_contents("C:\MAMP\htdocs\p4\app\database\MAAlbums.csv");
 	$rows = explode("\r\n", $albums);
-	print_r($rows);
+
+	$row = explode(",", $rows[1]);
+
+	for ($i = 0; $i<count($rows);$i++){
+		$album = explode(",", $rows[$i]);
+		print_r($album);
+		echo "<br>";
+
+		DB::insert("insert into albums (id, title, band_id, type, label, release_date)  values('$album[0]', '$album[1]', '$album[2]', '$album[3]', '$album[4]', '$album[5]')");
+		echo "added row to database?<br>";
+	}
+	
 
 });
