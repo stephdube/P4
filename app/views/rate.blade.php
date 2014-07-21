13755541f33de4fcece9e1e00bd7be693ea133f2
Steph Dube <scdube7@gmail.com>
2014-07-21 15:50:10 -0400

    trying laravel query functions instead of raw sql

--- app/views/rate.blade.php
+++ app/views/rate.blade.php
@@ -2,20 +2,22 @@
 
 @section('content')
 <?php
-	echo $album."<br>";
+	echo "You searched: <i>" . $album . "</i><br>";
 
 	//$rows= DB::table('albums')->where('title', '=', $album)->get();
 
-	$rows = DB::select("SELECT title, name FROM albums a
-		JOIN bands b ON b.id = a.band_id
-		WHERE a.title = 'Nightfall'");
+	//$rows = DB::select("SELECT title, name FROM albums a
+	//	JOIN bands b ON b.id = a.band_id
+	//	WHERE a.title = ?", array($album));
+
+	$rows = DB::table('albums')->join('bands', 'bands.id', '=', 'albums.band_id')
+		->select('title', 'name')->where('title', "=", $album)->get();
 
 	echo Pre::render($rows);
 
 	foreach ($rows as $row){
 		echo $row->title;
 	}
-
 
 ?>
 
